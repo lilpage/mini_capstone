522f7ea129676a7588d07d5a2fb8124cc021241c
Lil <lil@Lils-MacBook-Air.local>
2021-03-14 17:30:05 -0500

    Adds associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category_product
+  belongs_to :supplier
+  has_many :orders
+  has_many :categories, through: :category_product
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_193314) do
+ActiveRecord::Schema.define(version: 2021_03_14_213810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 2021_03_12_193314) do
   create_table "category_products", force: :cascade do |t|
     t.integer "product_id"
     t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "product_id"
+    t.integer "user_id"
+    t.decimal "total", precision: 9, scale: 2
+    t.decimal "subtotal", precision: 9, scale: 2
+    t.decimal "tax", precision: 9, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
